59e29e76ba5ce30f3c76d2c41af635d4d52adbd3
Mayte Mozo <mayte.mozoreyes@cfusacorp.com>
2024-07-11 08:26:51 -0700

    always show tooltip

--- src/Rangeslider.js
+++ src/Rangeslider.js
@@ -298,7 +298,7 @@ class Slider extends Component {
     const coords = this.coordinates(position)
     const fillStyle = { [dimension]: `${coords.fill}px` }
     const handleStyle = { [direction]: `${coords.handle}px` }
-    let showTooltip = tooltip && active
+    let showTooltip = tooltip
 
     let labelItems = []
     let labelKeys = Object.keys(labels)
